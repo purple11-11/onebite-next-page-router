98c77783b3d41a27ad0b5def5eee65fcfbd671b6
young <purple11_11@naver.com>
2024-09-07 14:20:00 +0900

    2.5)API Routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickButton = () => {
     router.push("/test");
   };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []); // 마운트 되었을 때 한 번 만 동작하기 때문에 deps 배열을 빈 배열로 함
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>
+          {/* <Link>태그 일 때 prefetch 되지 않도록 설정*/}
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
